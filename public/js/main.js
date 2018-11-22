26a5c377cfbe1c2dc448d874a4bf1b4e911213ee
langdonf <lfroker@gmail.com>
2018-11-22 15:19:36 -0800

    working on carousel

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,64 @@
+
+
+var section1 = function(){
+  $('body').removeClass()
+  $('body').addClass('first-color')
+  $('#about').addClass('hidden_left')
+  $('#about').removeClass('about')
+  $('#me2').addClass('hidden_right')
+  $('#me2').removeClass('me2')
+  $('#arr').removeClass('hide')
+}
+var section2 = function(){
+  $('body').removeClass()
+  $('body').addClass('second-color')
+  $('#about').removeClass('hidden_left')
+  $('#about').addClass('about')
+  $('#me2').removeClass('hidden_right')
+  $('#me2').addClass('me2')
+  $('#arr').addClass('hide')
+}
+var section3 = function(){
+  $('#about').addClass('hidden_left')
+  $('#about').removeClass('about')
+  $('#me2').addClass('hidden_right')
+  $('#me2').removeClass('me2')
+  $('body').removeClass()
+  $('body').addClass('third-color')
+}
+var section4 = function(){
+  $('body').removeClass()
+    $('body').addClass('fourth-color')
+}
+var section5 = function(){
+  $('body').removeClass()
+  $('body').addClass('fifth-color')
+}
+
+
+
+
+
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
-  if (scroll <= 900){
-    $('body').removeClass()
-    $('body').addClass('first-color')
-  } if (scroll >= 901  && scroll <= 1800){
-    $('body').removeClass()
-    $('body').addClass('second-color')
-  } if (scroll >= 2701 && scroll <= 3600){
-    $('body').removeClass()
-    $('body').addClass('third-color')
-  } if (scroll >= 4501 && scroll <= 5400){
-    $('body').removeClass()
-    $('body').addClass('fourth-color')
-  } if (scroll >= 6300){
-    $('body').removeClass()
-    $('body').addClass('fifth-color')
+  console.log(scroll);
+  if (scroll <= 250){
+    section1()
+  } if (scroll >= 250  && scroll <= 1250){
+    section2()
+  } if (scroll >= 1251 && scroll <= 1500){
+    section3()
+  } if (scroll >= 2101 && scroll <= 2800){
+    section4()
+  } if (scroll >= 2800){
+    section5()
   }
 });
 
-var num = 550; //number of pixels before modifying styles
+var num1 = 700; 
 
 $(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
+    if ($(window).scrollTop() > num1) {
         $('.nav').addClass('sticky');
     } else {
         $('.nav').removeClass('sticky');
@@ -29,37 +66,38 @@ $(window).bind('scroll', function () {
 });
 
 
+
+
+
+
+
+
+
+
+//////////////////////////http://www.goat1000.com/tagcanvas.php////////////////////////
 window.onload = function() {
   try {
     TagCanvas.Start('myCanvas');
   } catch(e) {
-    // something went wrong, hide the canvas container
     document.getElementById('myCanvasContainer').style.display = 'none';
   }};
 
-  $(document).ready(function() {
-    if( ! $('#myCanvas').tagcanvas({
-    
-    })) {
-      // TagCanvas failed to load
-      $('#myCanvasContainer').hide();
-    }
-    // your other jQuery stuff here...
-  });
-
-  window.onload = function() {
+window.onload = function() {
     // set colour of text and outline of active tag
     TagCanvas.minSpeed = .005
     TagCanvas.textColour = '#ffffff';
     TagCanvas.outlineColour = 'transparent';
+    TagCanvas.textFont = "'Nunito Sans', sans-serif";
+    TagCanvas.wheelZoom = false;
+    TagCanvas.initial = [.03,.05]
     TagCanvas.Start('myCanvas');
-   
-    
-   };
+
+};
 
 
+////////////////////////////http://www.goat1000.com/tagcanvas.php/////////////////////////////////
 
-
-
-
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
 
